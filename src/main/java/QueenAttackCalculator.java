0330bb3f086e18c2d8f464c8f8d1ac4824d3e7b0
Zafaripour <toastyhatesthesea@gmail.com>
2021-07-29 04:34:57 -0700

    Made board class and piece class and queen class

--- src/main/java/QueenAttackCalculator.java
+++ src/main/java/QueenAttackCalculator.java
@@ -13,9 +13,11 @@ Please remove this comment when submitting your solution.
 public class QueenAttackCalculator
 {
 
+    private Board theBoard;
+
     public QueenAttackCalculator(Queen firstQueen, Queen secondQueen)
     {
-
+        theBoard = new Board(8, 8);
     }
 
     public boolean canQueensAttackOneAnother()
@@ -29,10 +31,99 @@ public class QueenAttackCalculator
 }
 
 
-class Queen
+class Queen extends Piece
 {
+
+    private Board aBoard;
+    private int row, column;
+
+
     public Queen(int aRow, int aColumn)
     {
 
+        aBoard = new Board(8, 8);
+
+
     }
+
+    @Override
+    public void assignAttack()
+    {
+        super.assignAttack();
+
+
+    }
+
+    private void assignRowAttack()
+    {
+        for(int i=0; i<aBoard.getRows(); i++)
+        {
+            if (aBoard.getBoard()[row][i] == null || i != column)
+            {
+                aBoard.getBoard()[row][i] = new Covered();
+            }
+        }
+
+        //TODO Assign columns for attack and test row and columns for attacks
+    }
+}
+
+class Board
+{
+    private Piece[][] board;
+    private int rows, columns;
+
+    public Board(int rows, int columns)
+    {
+        this.rows = rows;
+        this.columns = columns;
+
+        this.board = new Piece[rows][columns];
+
+    }
+
+    public Piece[][] getBoard()
+    {
+        return board;
+    }
+
+    public void setBoard(Piece[][] board)
+    {
+        this.board = board;
+    }
+
+    public int getRows()
+    {
+        return rows;
+    }
+
+    public void setRows(int rows)
+    {
+        this.rows = rows;
+    }
+
+    public int getColumns()
+    {
+        return columns;
+    }
+
+    public void setColumns(int columns)
+    {
+        this.columns = columns;
+    }
+}
+
+class Piece
+{
+    public void assignAttack()
+    {
+
+    }
+
+
+}
+
+class Covered extends Piece
+{
+
 }
