9903efaf49c22b4682d9b31f3cfb10c803bb6d7f
Zafaripour <toastyhatesthesea@gmail.com>
2021-07-29 03:02:46 -0700

    First

---
+++ src/main/java/QueenAttackCalculator.java
@@ -0,0 +1,38 @@
+/*
+
+Since this exercise has a difficulty of > 4 it doesn't come
+with any starter implementation.
+This is so that you get to practice creating classes and methods
+which is an important part of programming in Java.
+
+Please remove this comment when submitting your solution.
+
+*/
+
+
+public class QueenAttackCalculator
+{
+
+    public QueenAttackCalculator(Queen firstQueen, Queen secondQueen)
+    {
+
+    }
+
+    public boolean canQueensAttackOneAnother()
+    {
+        boolean answer = false;
+
+        return answer;
+    }
+
+
+}
+
+
+class Queen
+{
+    public Queen(int aRow, int aColumn)
+    {
+
+    }
+}
